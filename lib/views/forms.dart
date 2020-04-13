38bd0621846f95cf3dc486431adbeea99e63c6b6
CharbelFr <charbel.g.frangieh@gmail.com>
2020-04-13 21:50:46 +0300

    joomla json pdfs forms

--- lib/views/forms.dart
+++ lib/views/forms.dart
@@ -1,10 +1,12 @@
 import 'dart:io';
 import 'dart:typed_data';
+import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:baladiyeh/widgets/pdfViewer.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:http/http.dart' as http;
 
 
 // Need to work on printing
@@ -13,15 +15,29 @@ class Forms  extends StatefulWidget {
   _FormsState createState() => _FormsState();
 }
 class _FormsState extends State<Forms> {
-  final _formList = [["Municipal Clearance Request", 'assets/pdforms/sample.pdf'], ["Lease Registration", 'assets/pdforms/sample.pdf'], ["Municipal Fees Installment Request", 'assets/pdforms/sample.pdf'], ["Occupancy License", 'assets/pdforms/sample.pdf'], ["Building Completion", 'assets/pdforms/sample.pdf'], ["Statement of Occupancy", 'assets/pdforms/sample.pdf'], ["Property Transfer declaration", 'assets/pdforms/sample.pdf']];
-  Future<String> prepareTestPdf(href) async {
-    final ByteData bytes = await DefaultAssetBundle.of(context).load(href);
-    final Uint8List list = bytes.buffer.asUint8List();
-    final tempDir = await getTemporaryDirectory();
-    final tempDocumentPath = '${tempDir.path}/$href';
-    final file = await File(tempDocumentPath).create(recursive: true);
-    file.writeAsBytesSync(list);
-    return tempDocumentPath;
+  Future<List<AppForm>> _getForms() async {
+    var data = await http.get('https://baladiyeh.joomla.com//submissions_forms.txt');
+    var jsonData = json.decode(data.body);
+    List<AppForm> forms = [];
+    for (var u in jsonData) {
+      AppForm form =
+          AppForm(u['Name'], u['File']);
+      forms.add(form);
+    }
+    return forms;
+  }
+  Future <File> getFilefromURL(String url) async {
+    try{
+      var data = await http.get(url);
+      var bytes = data.bodyBytes;
+      var dir = await getApplicationDocumentsDirectory();
+      File file = File('${dir.path}/form.pdf');
+      File urlFile = await file.writeAsBytes(bytes);
+      return urlFile;
+
+    } catch (e) {
+      throw Exception("Error opening url file");
+    }
   }
 
   @override
@@ -33,30 +49,47 @@ class _FormsState extends State<Forms> {
         ),
         centerTitle:true,
       ),
-      body: ListView.separated(
-        itemCount: _formList.length,
-        padding: const EdgeInsets.all(20.0),
-        itemBuilder: (context, index) {
-          return GestureDetector(
-            onTap: () {
-              prepareTestPdf(_formList[index][1]).then((path) {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => FullPdfViewerScreen(path, _formList[index][0])),
-                );
-              });
-            },
-            child: Container(
-              padding: EdgeInsets.all(18.0),
-              child: Text(_formList[index][0]),
-            ),
-          );
-        },
-        separatorBuilder: (context, index) {
-          return Divider(color: Colors.grey);
-        },
+      body: Container(
+        child: FutureBuilder(
+          future: _getForms(),
+          builder: (BuildContext context, AsyncSnapshot snapshot) {
+            if (snapshot.data == null) {
+              return Container(child: Center(child: Text('Loading News . . . ')));
+            } 
+            else {
+              return ListView.separated(
+                itemCount: snapshot.data.length,
+                padding: const EdgeInsets.all(20.0),
+                itemBuilder: (context, index) {
+                  return GestureDetector(
+                    onTap: () {
+                      getFilefromURL(snapshot.data[index].pdf).then((p) {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => FullPdfViewerScreen(p.path, snapshot.data[index].title)),
+                        );
+                      });
+                    },
+                    child: Container(
+                      padding: EdgeInsets.all(18.0),
+                      child: Text(snapshot.data[index].title),
+                    ),
+                  );
+                },
+                separatorBuilder: (context, index) {
+                  return Divider(color: Colors.grey);
+                },
+              );
+            }
+          }
+        ),
       ),
     );
   }
+}
+class AppForm{
+  String title;
+  String pdf;
+  AppForm(this.title, this.pdf);
 }
